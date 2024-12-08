3c30e344354966fa82dc698408cdd46ce0519d6d
AMSAGUINE Oualid <oualidams13@gmail.com>
2024-12-08 15:04:56 +0100

    1Commmit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,15 +2,20 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { authGuard } from './guards/auth.guard';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-        canActivate: [authGuard]
+        ///canActivate: [authGuard]
     },
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'sidebar',
+        component: SidebarComponent
     }
 ];
