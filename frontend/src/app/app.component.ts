2b918203f60c5596fcb4c4c79574eab1bae29f46
senshi <senshi@senshi.senshi>
2024-12-12 22:04:04 +0100

    Layout of If is resolved

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,31 +5,33 @@ import { HeaderComponent } from './component/header/header.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
 import { filter } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    FooterComponent,
-    HeaderComponent,
-    SidebarComponent,
-],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+	selector: 'app-root',
+	standalone: true,
+	imports: [
+		CommonModule,
+		RouterOutlet,
+		FooterComponent,
+		HeaderComponent,
+		SidebarComponent,
+	],
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'frontend';
-  isLoginRoute: boolean = false;
+	title = 'Event Driven TMS';
 
-  constructor(private router: Router) {}
+	constructor(private router: Router, private authService: AuthService) { }
 
-  ngOnInit() {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: NavigationEnd) => {
-      this.isLoginRoute = event.urlAfterRedirects.includes('/login');
-    });
-  }
+	get isAuthenticated(): boolean{
+		return this.authService.isLoggedIn();
+	}
+	get isDashboardRoute(): boolean{
+		return !this.router.url.startsWith('/login');
+	}
+	get isLoginRoute(): boolean{
+		return this.router.url.startsWith('/login');
+	}
 }
